e0301e4403136978614a8d36a993cadbc754d135
harshavardhanck123 <156813758+harshavardhanck123@users.noreply.github.com>
2024-01-17 20:59:54 +0530

    Add files via upload

---
+++ V3.js
@@ -0,0 +1,7 @@
+document.getElementById('acceptButton').addEventListener('click', function() {
+    alert('I am so happy you said yes! 😊');
+});
+
+document.getElementById('rejectButton').addEventListener('click', function() {
+    alert('No worries! Let me know if you change your mind. 😊');
+});
